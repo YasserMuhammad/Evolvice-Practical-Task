4e4b3676822a2d66c5739d633dab02ed5b22f670
Yasser Muhammad <myasser612@gmail.com>
2018-11-11 18:18:03 +0200

    Update function Name in Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
   constructor(private chatService: ChatService) { }
 
   ngOnInit() {
-    this.lastMessage = this.chatService.lastMessage();
+    this.lastMessage = this.chatService.getLastMessage();
   }
 
 }
